5ecd2947e8b9e022d5266d90327402e83b0203e8
tonicardenas@gmail.com <tonicardenas@gmail.com>
2011-06-10 20:15:45 +0200

    He metido el enunciado del problema en el archivo.

--- 0_sample.py
+++ 0_sample.py
@@ -1,5 +1,17 @@
 #! /usr/bin/env python
 
+# Your amazing program should sum the numbers given in each line, and one line for each question with the response. Note that some numbers might be negative.
+#
+# Sample input:
+#
+# 2 3
+# 4 5 1
+#
+# Sample output:
+#
+# 5
+# 10
+
 import sys
 
 for i in sys.stdin:
